e53d52119c7960dafe876eb24a12c30bbd7b4192
guyuuuuu <gybj2396@hotmail.com>
2022-09-01 21:28:45 -0400

    feat/polymorphic and container

---
+++ 8_container.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+int main() {
+    //operator overloading
+    string x = 'abc';
+    string y = 'abc';
+    string z = x + y;
+    //compiler see s operator+(x, y);
+
+    //a container is an adt whose propose is to hold other objects.
+
+    //steps for designing an adt
+    //1.establish an abstrction
+    //  what we want to do
+    //  define a set of public functions
+    //2.implement
+    //  figure out a data representation (private data members)
+    //  implement our public function
+}
